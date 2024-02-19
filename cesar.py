9d2795823558181ca2325eae0ebacb92dd3af94e
Taida Carmona <taidacr@hotmail.com>
2024-02-19 22:01:02 +0100

    Tenemos el algoritmo basico

--- cesar.py
+++ cesar.py
@@ -1,7 +1,20 @@
 class Cesar:
+    alfabeto=list("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ")
     def __init__(self,clave):
         if isinstance(clave,int):
             self.clave=clave
         else:
             raise AttributeError(f'{clave} debe ser un entero')
-        
+    
+      
+    def encriptar(self,mensaje):
+        mensaje_encriptado="" 
+        for caracter in mensaje:
+            pos=self.alfabeto.index(caracter)
+            pos2=pos+self.clave
+            new_caracter=self.alfabeto[pos2]
+            mensaje_encriptado+=new_caracter
+        return mensaje_encriptado
+    
+
+            
--- test_cesar.py
+++ test_cesar.py
@@ -1,10 +1,15 @@
 from cesar import Cesar
+import pytest
 
 def test_create_cesar():
     cesar=Cesar(3)
     assert cesar.clave==3
 
 def test_clave_must_be_integer():
+    with pytest.raises(AttributeError) as exc:
+        Cesar("algo que no es entero")
+    assert "debe ser un entero" in str(exc.value)
+
     try:
         cesar=Cesar("algo que no es un entero")
         variable_de_comprobacion= 0
@@ -13,3 +18,7 @@ def test_clave_must_be_integer():
         variable_de_comprobacion= 1
 
     assert variable_de_comprobacion== 1 #Se ha lanzado un AttributeError
+   
+def test_encriptar():
+    cesar=Cesar (3)
+    assert cesar.encriptar("HOLA")=="KRÑD"
